bade54f510c80b545ad538254fa2d1e520b09513
Ashish Deora <ashish.deora@greatplacetowork.com>
2020-10-25 17:29:25 +0530

    first commit

---
+++ BlazorServer/Pages/Counter.razor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServer.Pages
+{
+    public partial class Counter
+    {
+        private int currentCount = 0;
+
+        private string font_family = "Verdana";
+
+        private void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
